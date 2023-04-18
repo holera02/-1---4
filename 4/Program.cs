2760a793750b255c2dc1eac04ed4cc1de557fa76
STICH <STICH@DESKTOP-VD7MVFE>
2023-04-18 13:21:11 +0300

    Добавьте файлы проекта.

---
+++ 4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите номер дня в году (1-365): ");
+        int dayNum = int.Parse(Console.ReadLine());
+
+        int dayOfWeek = (dayNum + 5) % 7;
+
+        if (dayOfWeek == 0)
+        {
+            dayOfWeek = 7;
+        }
+
+        Console.WriteLine("Номер дня недели: " + dayOfWeek);
+
+        Console.ReadKey();
+    }
+}
